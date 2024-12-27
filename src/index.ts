db44ffcbc405f898ac922eed980f62205102e5c4
adisonsg <146038953+adisonsg@users.noreply.github.com>
2024-12-27 12:44:27 -0300

    Seção 01 - Introdução ao TS

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+function component() {
+    const element = document.getElementById('content');
+
+    element.innerHTML = "<h1>Olá mundo!</h1>"
+    element.innerHTML += "<h1>Olá mundo!</h1>"
+    element.innerHTML += "<h1>Olá mundo!</h1>"
+    element.innerHTML += "<h1>Olá mundo!</h1>"
+    element.innerHTML += "<h1>Olá mundo!</h1>"
+    element.innerHTML += "<h1>Olá mundo!</h1>"
+
+    return element;
+  }
+
+component();
+
+  
